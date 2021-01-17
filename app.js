c4029e38b3c5cf1149ac293f231774596ce89009
Shantanu001 <sjshantanu001@gmail.com>
2021-01-18 00:58:27 +0530

    added sell form api

--- app.js
+++ app.js
@@ -12,6 +12,14 @@ const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://Shantanu:bb0pD4SFebUd72gd@cluster0-fzxvj.mongodb.net/<dbname>?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true });
 var ObjectId = require('mongodb').ObjectId; 
+var cors = require('cors')
+var bodyParser = require('body-parser')
+
+var jsonParser = bodyParser.json({limit: '50mb'})
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+
+app.use(cors())
 
 // client.connect(err => {
 //   const collection = client.db("test").collection("devices");
@@ -25,10 +33,20 @@ let options = {
 };
 let geoCoder = nodeGeocoder(options);
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+
+//     // Request methods you wish to allow
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+//     // Request headers you wish to allow
+//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+//     // Set to true if you need the website to include cookies in the requests sent
+//     // to the API (e.g. in case you use sessions)
+//     res.setHeader('Access-Control-Allow-Credentials', true);
+//   next();
+// });
 
 app.get("/getLocation", async (req, res) => {
   // let requestPromise = Util.promisify(Request);
@@ -91,6 +109,30 @@ app.get("/getCategoryList", async (req, res) => {
   let categoryList = ["Cars","Bikes","Furniture","Jobs","Electronics&Appliances","Fashion","Mobiles","Services","Properties&Rent","Pets","Books,Sports&Hobbies","Others"];
   res.send({"data":categoryList,"status":200});
 });
+
+app.post("/addProductToSell",jsonParser,(req,res)=>{
+  console.log("req",req.body);
+  let data = req.body;
+  MongoClient.connect(uri,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  },function(err, client) {
+    if (err) throw err;
+    var db = client.db("idhardekho");
+    
+    db.collection("Product").insertOne(data,(function(err, result) {
+      //console.log("here",err,result);
+      if (err) throw err;
+      //console.log("123",result);
+      client.close();
+      res.send({
+        "msg":"Data inserted successfully",
+        "status":200
+      });
+    }));
+  });
+  //res.send(JSON.stringify(req));
+})
 
 app.listen(port, () =>{
   console.log(`Example app listening at http://localhost:${port}`);
